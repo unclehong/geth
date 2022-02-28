de5f49488f55371c0fc1bd5e87cd53d3e0a5baeb
unclehong <freekeywords@163.com>
2022-02-28 19:12:46 +0400

    a

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.8.0;
 
+//matemask
+// hedgehog person leopard smart trick gift gym priority outer target scare warrior
+// 0xa584A4e11Bc8a09Dfa5cAD31446e315DC7273dFB
+
 // geth --datadir="/home/ming/Developer/blockchain" --port 30001 --http --dev
 // geth attach /home/ming/Developer/blockchain/geth.ipc
-
+// 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
 contract Hello {
     function Hello(){
 
