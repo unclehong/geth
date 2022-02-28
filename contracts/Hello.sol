31de6e8bfe485c801793618d65239cf7892762db
unclehong <freekeywords@163.com>
2022-02-28 19:08:58 +0400

    a

---
+++ contracts/Hello.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+// geth --datadir="/home/ming/Developer/blockchain" --port 30001 --http --dev
+// geth attach /home/ming/Developer/blockchain/geth.ipc
+
+contract Hello {
+    function Hello(){
+
+    }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/unclehong/geth
+
+go 1.17
